5fb5e1672989f12dfd5f6171e941b116eb90a54b
danpeleg <danpeleg04@gmail.com>
2025-12-22 19:44:00 -0800

    fix

--- app/workspace/projects/[id]/page.tsx
+++ app/workspace/projects/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { ProjectDetailView } from "../ProjectDetailView";
