52fe9cf032bb20531c9f23ec8ab11877551ec148
danpeleg <danpeleg04@gmail.com>
2025-12-24 14:58:46 -0800

    Removed `google_tokens` table and updated schema definitions. Configured SSL conditionally based on environment.

--- db/db.ts
+++ db/db.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const db = drizzle({
     schema,
     connection: {
         connectionString: process.env.DATABASE_URL!,
-        ssl: {rejectUnauthorized: process.env.NODE_ENV === "production"}
+        ssl: isProd ? { rejectUnauthorized: true } : false,
     },
 });
