55623081b7c1e6e43e9518caf50f39e68b86813c
danpeleg <danpeleg04@gmail.com>
2026-01-09 19:41:15 -0800

    Added livekit for meetings.

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -11,27 +11,16 @@ interface MeetingTypeInput {
     duration: number;
     description: string;
     color: string;
-    platform: string;
     active: boolean;
     maxBookings?: number;
 }
 
 const initialMeetingTypes: MeetingTypeInput[] = [
     {
-        name: "Zoom",
+        name: "Online Meeting",
         duration: 30,
         description: "Initial consultation to understand client needs and explore how Virevos can help",
         color: "blue",
-        platform: "zoom",
-        active: true,
-        maxBookings: 3,
-    },
-    {
-        name: "Google Meet",
-        duration: 30,
-        description: "Comprehensive onboarding session for new clients",
-        color: "green",
-        platform: "google-meet",
         active: true,
         maxBookings: 3,
     },
@@ -40,10 +29,9 @@ const initialMeetingTypes: MeetingTypeInput[] = [
         duration: 30,
         description: "Initial consultation to understand client needs and explore how Virevos can help",
         color: "purple",
-        platform: "In-Person",
         active: true,
         maxBookings: 3,
-    }
+    },
 ];
 
 export async function POST(req: Request) {
@@ -51,7 +39,6 @@ export async function POST(req: Request) {
 
     if (evt.type === "user.created") {
         const { id, email_addresses, first_name, last_name } = evt.data;
-        //console.log(evt.data);
         await db.insert(users).values({
             user_id: id,
             email: email_addresses[0]?.email_address || "",
