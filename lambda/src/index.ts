55a4093adf7db15ad893242396ac4d100a54bb8b
danpeleg <danpeleg04@gmail.com>
2026-01-31 08:29:04 -0800

    Add user insertion logic to Lambda handler

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,7 +1,23 @@
+import {events, users} from "@/db/schema";
+import { db } from "@/db/db";
+import { eq } from "drizzle-orm";
+
 /*
 virevos-schedule-meeting lambda function
 */
 export const handler = async (event: any) => {
     console.log("User Id: ")
     console.log(event.userId);
+    try {
+        //await db.update(events).set({
+        //    status: "active"
+        //}).where(eq(events.userId, event.userId));
+        await db.insert(users).values({
+            user_id: event.userId,
+            name: "John",
+            email: "john@gmail.com"
+        })
+    } catch (error) {
+        console.error(error);
+    }
 };
