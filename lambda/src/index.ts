cd79c1ea94cd4b388a38a3d75f0b442ebfa47327
danpeleg <danpeleg04@gmail.com>
2026-01-31 11:37:58 -0800

    Update esbuild config in `lambda` package.json to include dotenv config and clean up `index.ts` comments

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -12,7 +12,6 @@ export const handler = async (event: any) => {
         //await db.update(events).set({
         //    status: "active"
         //}).where(eq(events.userId, event.userId));
-        //
         await db.insert(users).values({
             user_id: event.userId,
             name: "John",
