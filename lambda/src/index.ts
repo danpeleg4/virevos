e641a079495ae3bf88708b1443f9e8e5dc523b3c
danpeleg <danpeleg04@gmail.com>
2026-01-31 18:38:11 -0800

    Simplify GitHub Actions and remove unused `dotenv`, `drizzle-orm`, and `pg` dependencies from `lambda`

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,5 +1,6 @@
-import {events, users} from "@/db/schema";
+import { events } from "@/db/schema";
 import { db } from "@/db/db";
+import { eq } from "drizzle-orm";
 
 /*
 virevos-schedule-meeting lambda function
@@ -8,15 +9,9 @@ export const handler = async (event: any) => {
     console.log("User Id: ")
     console.log(event.userId);
     try {
-        //await db.update(events).set({
-        //    status: "active"
-        //}).where(eq(events.userId, event.userId));
-        //
-        await db.insert(users).values({
-            user_id: event.userId,
-            name: "John",
-            email: "john@gmail.com"
-        })
+        await db.update(events).set({
+            status: "active"
+        }).where(eq(events.id, event.id));
     } catch (error) {
         console.error(error);
     }
