7e713d2c670a829b6da081c351cf6c503d450b80
danpeleg <danpeleg04@gmail.com>
2026-02-06 12:40:27 -0800

    lambda test.

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -9,6 +9,7 @@ export const handler = async (event: any) => {
     console.log("User Id: ")
     console.log(event.userId);
     try {
+        const h = 1
         await db.update(events).set({
             status: "active"
         }).where(eq(events.id, event.id));
