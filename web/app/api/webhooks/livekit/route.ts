fc57f72bb0b9d91a7063aed96ab3b09081c82dab
danpeleg <danpeleg04@gmail.com>
2026-03-07 18:14:04 -0800

    updates

--- web/app/api/webhooks/livekit/route.ts
+++ web/app/api/webhooks/livekit/route.ts
@@ -81,7 +81,7 @@ export async function POST(req: NextRequest) {
             value: {
               accessKey: process.env.AWS_LIVE_KIT_S3_ACCESS_KEY,
               secret: process.env.AWS_LIVE_KIT_S3_SECRET_KEY,
-              bucket: "virevos-recordings-dev",
+              bucket: process.env.AWS_BUCKET_NAME,
               region: process.env.AWS_REGION,
               forcePathStyle: true,
             },
